c90aa4eb2eb673fbb3e37cd37daa30064daf44d6
AnshulGaikwad976 <gaikwad.anshul14@gmail.com>
2025-10-28 18:50:47 +0200

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,12 @@
+set1 = {"green" , "blue"}
+set2 = {"blue" , "yeloe"}
+print("original set")
+print(set1)
+print(set2)
+serc = set1.union(set2)
+print("/nUnion of above sets;")
+print(serc)
+
+set3 = set1.intersection(set2)
+print("intersection set:" )
+print(set3)
